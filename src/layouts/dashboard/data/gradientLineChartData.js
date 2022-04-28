744b65d162849f23ff3d77c96e3476ec7f2a1d99
topfreelancer828 <shywaewajane@gmail.com>
2022-04-29 01:16:52 +0900

    fix first page gradient chart

--- src/layouts/dashboard/data/gradientLineChartData.js
+++ src/layouts/dashboard/data/gradientLineChartData.js
@@ -14,18 +14,18 @@ Coded by www.creative-tim.com
 */
 
 const gradientLineChartData = {
-  labels: ["1-1", "1-2", "1-3", "2-1", "2-2", "2-3", "3-1", "3-2", "3-3"],
+  labels: ["1", "2", "3"],
   datasets: [
     {
-      label: "Real Drop",
+      label: "Successful Wallets",
       color: "info",
-      data: [400, 29500, 100, 500, 29500, 0, 500, 29500, 0],
+      data: [562, 556, 573],
     },
-    // {
-    //   label: "Planed Drop",
-    //   color: "dark",
-    //   data: [0, 30000, 0, 0, 30000, 0, 0, 30000, 0],
-    // },
+    {
+      label: "Expect",
+      color: "dark",
+      data: [563, 563, 563],
+    },
   ],
 };
 
